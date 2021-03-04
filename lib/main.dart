b9366e5aad21d8b8f4a2e8997ffee0100e554fb2
jcss1462 <jcss1462@gmail.com>
2021-03-04 02:53:44 -0500

    conexion con firebase comenzada

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:reciclaje_app/core/constants.dart';
 import 'package:reciclaje_app/core/routers.dart';
-void main() {
+import  'package:firebase_core/firebase_core.dart';
+import  'package:reciclaje_app/page/index.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(ReciclajeApp());
 }
 
 class ReciclajeApp extends StatelessWidget {
-  const ReciclajeApp({Key key}) : super(key: key);
-
+  final Future<FirebaseApp> _fbApp=  Firebase.initializeApp();
+  ReciclajeApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,7 +23,22 @@ class ReciclajeApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
         ),
         onGenerateRoute: Routers.generateRoute,
-        initialRoute: intialRoute,
+        //valido la coneccion con firebase
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError){
+              print('Error de inicializacion con firebase ${snapshot.error.toString()}');
+              return Text('Error de inicializacion con firebase');
+            }else if (snapshot.hasData){
+              return Login();
+            }else{
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } 
+          },
+        ),
       ),
     );
   }
