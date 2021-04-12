3e05d88d7f885fa973f5db42293f41d8a26539f1
Daniela Chocue <danielachocue2@gmail.com>
2021-04-12 15:21:46 -0500

    editar venta de residuo casi terminado

--- lib/page/pageEditar.dart
+++ lib/page/pageEditar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:reciclaje_app/data/datasources/carroVenta_datasource.dart';
+import 'package:reciclaje_app/data/model/tipoResiduo.dart';
 import 'package:reciclaje_app/data/model/ventas.dart';
-import 'package:reciclaje_app/page/carrodeVentas.dart';
+import 'package:reciclaje_app/page/index.dart';
 import 'package:reciclaje_app/service/preferences.dart';
 import 'package:reciclaje_app/widgets/navbar.dart';
 
@@ -17,6 +18,7 @@ class PageEditar extends StatefulWidget {
 class _PageEditarState extends State<PageEditar> {
   Preferences preferencias = new Preferences();
   int _idventa;
+  TipoResiduo tipoResiduo;
 
   final formKey = GlobalKey<FormState>();
   CarroVentasDataSourceImpl carroVentasDataSourceImpl =
@@ -83,59 +85,52 @@ class _PageEditarState extends State<PageEditar> {
                         return Text('Error: ${snapshot.error}');
                       } else {
                         this.ventas = snapshot.data;
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          child: SingleChildScrollView(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Edicion de Venta",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 25),
-                                ),
-                                Form(
-                                  key: formKey,
-                                  child: Column(
-                                    children: [
-                                      Card(
-                                        elevation: 5,
-                                        child: Container(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width -
-                                              80,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height /
-                                              5,
-                                          constraints: BoxConstraints(
-                                            minWidth: 150,
-                                            minHeight: 150,
-                                          ),
-                                          padding: EdgeInsets.only(
-                                              top: 20.0,
-                                              bottom: 30.0,
-                                              left: 40,
-                                              right: 40),
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            color: Colors.white,
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey
-                                                    .withOpacity(0.25),
-                                                spreadRadius: 5,
-                                                offset: Offset(0, 3),
-                                              ),
-                                            ],
-                                          ),
-                                          child: SingleChildScrollView(
+                        return Center(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width / 1.15,
+                            height: MediaQuery.of(context).size.height / 1.8,
+                            constraints: BoxConstraints(
+                              minWidth: 160,
+                              minHeight: 160,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.25),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                )
+                              ],
+                            ),
+                            child: Center(
+                              child: SingleChildScrollView(
+                                padding: EdgeInsets.only(
+                                    top: 5.0,
+                                    bottom: 30.0,
+                                    left: 40,
+                                    right: 40),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Edicion de Venta",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Color.fromRGBO(46, 99, 238, 1),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 27),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    Form(
+                                      key: formKey,
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            height: 70,
                                             child: Column(
                                               children: <Widget>[
                                                 Row(
@@ -154,7 +149,7 @@ class _PageEditarState extends State<PageEditar> {
                                                       ),
                                                     ),
                                                     Text(
-                                                      ventas.tipo.toString(),
+                                                      ventas.tipo,
                                                       textAlign: TextAlign.left,
                                                       style: TextStyle(
                                                         color: Color.fromRGBO(
@@ -165,16 +160,162 @@ class _PageEditarState extends State<PageEditar> {
                                                       ),
                                                     ),
                                                   ],
-                                                )
+                                                ),
                                               ],
                                             ),
                                           ),
+                                          new Container(
+                                            child: TextFormField(
+                                              initialValue: peso != null
+                                                  ? peso.toString()
+                                                  : "",
+                                              keyboardType:
+                                                  TextInputType.number,
+                                              decoration: InputDecoration(
+                                                  hintText: "Ingrese el Peso",
+                                                  contentPadding:
+                                                      EdgeInsets.all(11),
+                                                  enabledBorder:
+                                                      OutlineInputBorder(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(
+                                                                      30.0),
+                                                          borderSide:
+                                                              BorderSide(
+                                                            color:
+                                                                Color.fromRGBO(
+                                                                    46,
+                                                                    99,
+                                                                    238,
+                                                                    1),
+                                                            width: 0.5,
+                                                          ))),
+                                              onSaved: (value) {
+                                                peso = double.parse(value);
+                                                if (peso == 0) {
+                                                  total = 0;
+                                                } else {
+                                                  total = ventas.idventa * peso;
+                                                }
+                                              },
+                                              validator: (value) {
+                                                if (value.isEmpty) {
+                                                  return "Ingrese el peso a editar";
+                                                } else {
+                                                  return null;
+                                                }
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 15,
+                                    ),
+                                    new Column(
+                                      children: [
+                                        Text(
+                                          "Total de Venta del Residuo",
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  46, 99, 238, 1),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                        SizedBox(height: 25),
+                                        Text(
+                                          total == null
+                                              ? "\$0"
+                                              : "\$" + total.toString(),
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  46, 99, 238, 1),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 25,
+                                    ),
+                                    MaterialButton(
+                                      height: 50,
+                                      minWidth: 150,
+                                      color: Color.fromRGBO(46, 99, 238, 1),
+                                      textColor: Colors.white,
+                                      child: new Text(
+                                        "Editar Ventas",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                )
-                              ],
+                                      onPressed: () async {
+                                        formKey.currentState.save();
+                                        print(peso);
+                                        print(ventas.tipo);
+                                        print(total);
+
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) => AlertDialog(
+                                            title: Text(
+                                              "Residuo Editado",
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    46, 99, 238, 1),
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            content: Text(
+                                                "El Residuo se Editado Exitosamente"),
+                                            actions: <Widget>[
+                                              TextButton(
+                                                child: Text(
+                                                  'Ok',
+                                                  style: TextStyle(
+                                                    color: Color.fromRGBO(
+                                                        46, 99, 238, 1),
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 15,
+                                                  ),
+                                                ),
+                                                onPressed: () {
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              CarroDeVentas()));
+                                                },
+                                              ),
+                                              TextButton(
+                                                  child: Text(
+                                                    'Cancelar',
+                                                    style: TextStyle(
+                                                      color: Color.fromRGBO(
+                                                          46, 99, 238, 1),
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 15,
+                                                    ),
+                                                  ),
+                                                  onPressed: () {
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            PageEditar(ventas
+                                                                .idventa));
+                                                  }),
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                         );
