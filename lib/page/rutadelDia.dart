b2c3072c4940cb8dfaf668a62500b7f20b9a9ce4
Daniela Chocue <danielachocue2@gmail.com>
2021-05-26 17:53:37 -0500

    inicio de ruta

--- lib/page/rutadelDia.dart
+++ lib/page/rutadelDia.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 import 'package:reciclaje_app/blocs/application_bloc.dart';
 import 'package:reciclaje_app/data/datasources/recoleccionDonacion_datasource.dart';
@@ -17,11 +17,10 @@ class RutadelDia extends StatefulWidget {
 class _RutadelDiaState extends State<RutadelDia> {
   Preferences preferencias = new Preferences();
   String _email;
-  Location location = Location();
   GoogleMapController googleMapController;
-  Marker maker;
-  Circle circle;
-  bool isvisible = true;
+  List<Coordinates> coordinates = [];
+  Map<MarkerId, Marker> markers = {};
+
   final formKey = GlobalKey<FormState>();
   RecoleccionDonacionDataSourceImpl recoleccionDonacionDataSourceImpl =
       new RecoleccionDonacionDataSourceImpl();
@@ -39,10 +38,48 @@ class _RutadelDiaState extends State<RutadelDia> {
     });
   }
 
-  Future<CarrodeDonacionList> getListSolicitudesAgendadas() async {
-    return await this
-        .recoleccionDonacionDataSourceImpl
-        .recicladorCarrosAsignados(_email);
+  getCoordenadas(String address) async {
+    var coordenadas = await Geocoder.local.findAddressesFromQuery(address);
+    var direcion = coordenadas.first;
+    coordinates.add(direcion.coordinates);
+    print(direcion.coordinates);
+    print(coordinates);
+    return coordinates;
+  }
+
+  bool reload = false;
+  void initMarker() async {
+    for (var i = 0; i < coordinates.length; i++) {
+      final MarkerId markerId = MarkerId(markers.length.toString());
+      LatLng markerPos =
+          LatLng(coordinates[i].latitude, coordinates[i].longitude);
+      final Marker marker = Marker(
+        icon: BitmapDescriptor.defaultMarker,
+        markerId: markerId,
+        position: markerPos,
+      );
+      markers[markerId] = marker;
+    }
+    //Control para evitar el ciclo infinito
+    if (reload == false) {
+      setState(() {
+        reload = true;
+      });
+    }
+  }
+
+  getMakerData() async {
+    coordinates.clear();
+    return await recoleccionDonacionDataSourceImpl
+        .recicladorCarrosAsignados(_email)
+        .then((value) {
+      if (value.solicitudes.length != 0) {
+        for (int i = 0; i < value.solicitudes.length; i++) {
+          getCoordenadas(value.solicitudes[i].direccionRecoleccion);
+        }
+        initMarker();
+      }
+    });
   }
 
   @override
@@ -84,7 +121,7 @@ class _RutadelDiaState extends State<RutadelDia> {
                             return Text('Error: ${snapshot.error}');
                           } else {
                             return FutureBuilder(
-                              future: getListSolicitudesAgendadas(),
+                              future: getMakerData(),
                               builder: (BuildContext context,
                                   AsyncSnapshot snapshot) {
                                 switch (snapshot.connectionState) {
@@ -95,27 +132,37 @@ class _RutadelDiaState extends State<RutadelDia> {
                                       return Text('Error: ${snapshot.error}');
                                     } else {
                                       this.solicitudes = snapshot.data;
-                                      return Container(
-                                        width:
-                                            MediaQuery.of(context).size.width,
-                                        height:
-                                            MediaQuery.of(context).size.height,
-                                        child: Center(
-                                          child: GoogleMap(
-                                            mapType: MapType.normal,
-                                            myLocationEnabled: true,
-                                            initialCameraPosition:
-                                                CameraPosition(
-                                                    target: LatLng(
-                                                        applicationBolc
-                                                            .currentLocation
-                                                            .latitude,
-                                                        applicationBolc
-                                                            .currentLocation
-                                                            .longitude),
-                                                    zoom: 20),
-                                          ),
-                                        ),
+                                      return Stack(
+                                        children: [
+                                          Container(
+                                            height: MediaQuery.of(context)
+                                                .size
+                                                .height,
+                                            width: MediaQuery.of(context)
+                                                .size
+                                                .width,
+                                            child: GoogleMap(
+                                              markers: Set<Marker>.of(
+                                                  markers.values),
+                                              mapType: MapType.normal,
+                                              myLocationEnabled: true,
+                                              initialCameraPosition:
+                                                  CameraPosition(
+                                                      target: LatLng(
+                                                          applicationBolc
+                                                              .currentLocation
+                                                              .latitude,
+                                                          applicationBolc
+                                                              .currentLocation
+                                                              .longitude),
+                                                      zoom: 10),
+                                              onMapCreated: (GoogleMapController
+                                                  controller) {
+                                                controller = controller;
+                                              },
+                                            ),
+                                          )
+                                        ],
                                       );
                                     }
                                 }
