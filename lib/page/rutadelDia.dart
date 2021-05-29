1d50f2f2a75f90b0bde9590c28eaa6489bc73c5a
Daniela Chocue <danielachocue2@gmail.com>
2021-05-29 11:43:06 -0500

    mejoras de ruta del dia

--- lib/page/rutadelDia.dart
+++ lib/page/rutadelDia.dart
@@ -5,6 +5,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:reciclaje_app/blocs/application_bloc.dart';
 import 'package:reciclaje_app/data/datasources/recoleccionDonacion_datasource.dart';
+import 'package:reciclaje_app/data/model/aplicacionRecoleccion.dart';
 import 'package:reciclaje_app/data/model/carrodeDonacionList.dart';
 import 'package:reciclaje_app/service/preferences.dart';
 import 'package:reciclaje_app/widgets/dialogBox.dart';
@@ -57,20 +58,20 @@ class _RutadelDiaState extends State<RutadelDia> {
     return coordinates;
   }
 
-  Future<void> initMarker(List<Coordinates> listCoodinates) async {
+  Future<void> initMarker(
+      List<Coordinates> listCoodinates, CarrodeDonacionList listaCarros) async {
     print("Iniciando creacion de markers");
     for (var i = 0; i < listCoodinates.length; i++) {
       final MarkerId markerId = MarkerId(markers.length.toString());
       LatLng markerPos =
           LatLng(listCoodinates[i].latitude, listCoodinates[i].longitude);
       final Marker marker = Marker(
-        icon: BitmapDescriptor.defaultMarker,
-        markerId: markerId,
-        position: markerPos,
-        infoWindow: InfoWindow(onTap: () {
-          print("hola");
-        }),
-      );
+          icon: BitmapDescriptor.defaultMarker,
+          markerId: markerId,
+          position: markerPos,
+          onTap: () {
+            onCardInfo(listaCarros);
+          });
       markers[markerId] = marker;
     }
     print("Creacion de markers terminada");
@@ -86,7 +87,7 @@ class _RutadelDiaState extends State<RutadelDia> {
         print("Iniciando conversiona coordenadas");
         await getCoordenadas(listaCarros).then((listaCoordenadas) async {
           print("Conversion terminada");
-          await initMarker(listaCoordenadas);
+          await initMarker(listaCoordenadas, listaCarros);
         }).onError((error, stackTrace) {
           showDialog(
             context: context,
@@ -102,6 +103,150 @@ class _RutadelDiaState extends State<RutadelDia> {
             "Problema obteniendo la lista del carros deisponibles del back"),
       );
     });
+  }
+
+  Future<void> onCardInfo(CarrodeDonacionList listacarros) async {
+    for (int i = 0; i < listacarros.solicitudes.length; i++) {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding:
+                  EdgeInsets.only(top: 20.0, bottom: 20.0, left: 20, right: 20),
+              height: MediaQuery.of(context).size.height / 3,
+              constraints: BoxConstraints(
+                minWidth: 150,
+                minHeight: 230,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Theme.of(context).canvasColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.15),
+                    spreadRadius: 5,
+                    offset: Offset(0, 3),
+                  )
+                ],
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Text(
+                          "Usuario:",
+                          textAlign: TextAlign.left,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Color.fromRGBO(46, 99, 238, 1),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Row(children: [
+                      Text(
+                        listacarros.solicitudes[i].emailCivil,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Color.fromRGBO(46, 99, 238, 1),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 18,
+                        ),
+                      )
+                    ]),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Dirección: ",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromRGBO(46, 99, 238, 1),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Text(
+                            listacarros.solicitudes[i].direccionRecoleccion,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color.fromRGBO(46, 99, 238, 1),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    MaterialButton(
+                      height: 50,
+                      minWidth: 250,
+                      color: Color.fromRGBO(46, 99, 238, 1),
+                      textColor: Colors.white,
+                      child: new Text(
+                        "Agendar Visita",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                      onPressed: () async {
+                        print("email solicitante: " + _email);
+                        print("carrito de donacion: " +
+                            listacarros.solicitudes[i].idcarrodonacion
+                                .toString());
+
+                        AplicacionRecoleccion aplicacionRecoleccion =
+                            new AplicacionRecoleccion(
+                                listacarros.solicitudes[i].idcarrodonacion
+                                    .toInt(),
+                                _email);
+                        this
+                            .recoleccionDonacionDataSourceImpl
+                            .aplicacionaRecolectar(aplicacionRecoleccion)
+                            .then((value) {
+                          showDialog(
+                            context: context,
+                            builder: (context) => DialogBox(
+                                "Aplicación exitosa",
+                                "Esperar la aceptación del usuario civil"),
+                          ).then((value) {
+                            setState(() {});
+                          });
+                        }).onError((error, stackTrace) {
+                          showDialog(
+                            context: context,
+                            builder: (context) =>
+                                DialogBox("Error al Aplicar", error.toString()),
+                          );
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+    }
   }
 
   Future<void> setPolylines() async {
